bc80c0c9646183d6239fa072d14af271afb837cf
ThanWai <48476234+thanwaimdy@users.noreply.github.com>
2019-04-09 14:37:06 +0000

    Add files via upload

---
+++ 34 def module test import.py
@@ -0,0 +1,22 @@
+import shwe  # this shwe.py file is same path to import file
+
+shwe.kolar()
+
+shwe.one()
+
+shwe.two()
+
+shwe.three()
+
+goody = shwe.kolar # a def to variable 
+
+goody()    #  not to use def only use variabe
+
+#reload(shwe)   # reload not working
+
+
+
+
+
+
+
---
+++ 35 shwe.py
@@ -0,0 +1,20 @@
+# this is the module which i will be 
+#calling and using again and again
+
+def kolar():
+    
+    print("I am a new module which will work i am called")
+    
+# this file save with same python path with new file name
+
+def one():
+    print("quick black fox jumps over the lazy dog.")
+    
+
+def two():
+    print("1358086533 25799532 855874685")
+    
+def three():
+    print("  LEE LAR  &*%# #&&$#&*$@ #%*$#*#@")
+    
+
